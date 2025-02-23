5c0204922f532691d0c620ff18c08f8111ff8845
charlesnyanje <charleswilliam26@gmail.com>
2025-02-23 21:42:15 +0300

    created Expenses component

--- src/App.js
+++ src/App.js
@@ -112,32 +112,6 @@ function Home() {
   );
 }
 
-function Expenses() {
-  return (
-    <Container maxWidth="sm">
-      <Box
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-        minHeight="60vh"
-      >
-        <Paper
-          elevation={3}
-          sx={{ p: 4, textAlign: "center", borderRadius: 2 }}
-        >
-          <Typography variant="h4" color="primary" gutterBottom>
-            Expenses Overview
-          </Typography>
-          <Typography variant="body1" color="textSecondary">
-            Please select a category from the Budget to view details.
-          </Typography>
-        </Paper>
-      </Box>
-    </Container>
-  );
-}
-
 function App() {
   return (
     <Router>
---
+++ src/Expenses.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  useNavigate,
+  useParams,
+  useLocation,
+} from "react-router-dom";
+import {
+  Container,
+  Select,
+  MenuItem,
+  Card,
+  Box,
+  Chip,
+  CardContent,
+  Typography,
+  TextField,
+  Button,
+  List,
+  ListItem,
+  IconButton,
+  ListItemText,
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
+
+function Expenses() {
+  return (
+    <Container maxWidth="sm">
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        minHeight="60vh"
+      >
+        <Paper
+          elevation={3}
+          sx={{ p: 4, textAlign: "center", borderRadius: 2 }}
+        >
+          <Typography variant="h4" color="primary" gutterBottom>
+            Expenses Overview
+          </Typography>
+          <Typography variant="body1" color="textSecondary">
+            Please select a category from the Budget to view details.
+          </Typography>
+        </Paper>
+      </Box>
+    </Container>
+  );
+}
